c614e1049600ede11c030f3cc09443a5894f680c
Ariel Ortenzi <arielortenzi@gmail.com>
2024-09-05 09:02:25 -0300

    ultimo commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,12 +10,12 @@ function NavBar() {
                     <img className="size-40" src={logo} alt="Logo orpack" />
                 </div>
                 <div className="flex grow justify-center items-center">
-                    <input className="w-[60%] h-12 text-center rounded-lg border-2 font-quickSand" type="text" placeholder="¿Qué estas buscando?" />
+                    <input className="w-[60%] h-12 text-center rounded-lg border-2 font-quickSand text-lg font-semibold" type="text" placeholder="¿Qué estas buscando?" />
                     <button><FcSearch className="m-2" size="50px"/></button>
                     
                 </div>
                 <div className="flex items-center justify-around">
-                    <ul className="flex justify-around items-center space-x-10 font-bebas text-2xl ">
+                    <ul className="flex justify-around items-center space-x-10 font-bebas text-2xl">
                         <li>
                             <a className="hover:text-azulOrpack" href="#">Inicio</a>
                         </li>
