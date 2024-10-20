2df3ffb92f946e465e888885c3ba409d577fb2f8
Ariel Ortenzi <arielortenzi@gmail.com>
2024-10-20 05:30:38 -0300

    coreciones para despliegue vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import "../src/index.css"
-import NavBar from "./components/NavBar/NavBar"
-import ItemListContainer from "./components/itemListContainer/ItemListContainer"
-import ItemDetailContainer from "./components/itemDetailContainer/ItemDetailContainer"
-import CartList from "./components/cart/CartList"
-import NotFound404 from "./components/notFound404/NotFound404"
-import { CartProvider } from "./components/context/CartContext"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/itemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/itemDetailContainer/ItemDetailContainer";
+import CartList from "./components/cart/CartList";
+import NotFound404 from "./components/notFound404/NotFound404";
+import { CartProvider } from "./components/context/CartContext";
 
 
 function App() {
